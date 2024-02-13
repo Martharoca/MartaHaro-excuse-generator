f0171fa62865f0b4efd835139fec10928324fe7c
Marta Haro <143503919+Martharoca@users.noreply.github.com>
2024-02-13 06:55:09 +0000

    new icon

--- src/app.js
+++ src/app.js
@@ -3,9 +3,44 @@ import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "src/assets/img/berry.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = writeExcuse();
 };
+let writeExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "sat"];
+  let what = ["my homework", "the keys", "the car", "outside my door"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+};
+
+//write your code here
+// declaramos los arrays que me dan
+//obtener un valor random de cada array
+// concatenar los valores random para armar la oración
+//
+
+// console.log("Hello Rigo from the console!");
+// en el doc html obtengo un elemento a traves de su id
+// document.getElementById(“demo”).innerHTML = “aqui pongo la excusa que vaya a generar”;
